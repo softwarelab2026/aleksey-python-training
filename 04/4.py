9f8f82c29f31ec1258dbed88a79e36f539c97f83
Aleksey <alekseyprog1@gmail.com>
2026-03-10 22:29:51 +0200

    Completed ex4 in the Chapter 4

---
+++ 04/4.py
@@ -0,0 +1,33 @@
+def main():
+  num = int(input('Please enter a 5 digit number\n'))
+  print('You entered the number: {}'.format(num))
+  print('The digits of this number are:' + list_digits(num))
+  print('The sum of this digits is:' + sum_digits(num))
+
+
+def list_digits(num: int) -> str:
+  result = ''
+  power = count_digits(num)-1
+  while power > 0:
+    result += '{}, '.format(num // 10 ** power % 10)
+    power -= 1
+  result += str(num % 10)
+  return result
+
+
+def count_digits(num: int):
+  count = 0
+  while num > 0:
+    count += 1
+    num //= 10
+  return count
+
+def sum_digits(num: int):
+  sum_dig = 0
+  while num > 0:
+    sum_dig += num % 10
+    num //= 10
+  return sum_dig
+
+if __name__ == '__main__':
+  main()
